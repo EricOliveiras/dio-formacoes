5569582a5ce0360c01e52c8b0f1fb0b1fc5b3202
Eric Oliveira <oiericoi@hotmail.com>
2023-09-03 12:34:17 -0300

    feat: add educational content data

--- src/main/kotlin/database/Db.kt
+++ src/main/kotlin/database/Db.kt
@@ -1,5 +1,6 @@
 package database
 
+import models.EducationalContent
 import models.User
 
 val dataUser: MutableList<User> = mutableListOf()
@@ -21,5 +22,37 @@ fun getAllUsers(): List<User> {
 }
 
 fun countUsers(): Int {
-    return  dataUser.count()
+    return dataUser.count()
 }
+
+val dataEducationalContent: MutableList<EducationalContent> = mutableListOf()
+
+fun addEducationalContent(educationalContent: EducationalContent) {
+    dataEducationalContent.add(educationalContent)
+}
+
+fun addManyEducationalContent(vararg educationalContent: List<EducationalContent>) {
+    educationalContent.forEach { data -> dataEducationalContent.addAll(data) }
+}
+
+fun getEducationalContentById(id: Int): EducationalContent? {
+    return dataEducationalContent.find { it.id == id }
+}
+
+fun getAllEducationalContent(): List<EducationalContent> {
+    return dataEducationalContent.toList()
+}
+
+fun countEducationalContent(): Int {
+    return dataEducationalContent.count()
+}
+
+val educationalContentIntro: MutableList<EducationalContent> = mutableListOf(
+    EducationalContent(1, "Olá_Mundo"),
+    EducationalContent(2, "Funções"),
+    EducationalContent(3, "Variáveis"),
+    EducationalContent(4, "Variáveis"),
+    EducationalContent(5, "Null Safety"),
+    EducationalContent(6, "Classes"),
+    EducationalContent(7, "Generics"),
+)
