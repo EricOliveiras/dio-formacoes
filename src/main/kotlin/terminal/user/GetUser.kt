58af345de5f3acd966945225ec1def7c5e364be0
Eric Oliveira <oiericoi@hotmail.com>
2023-09-03 12:46:29 -0300

    feat: add get user

---
+++ src/main/kotlin/terminal/user/GetUser.kt
@@ -0,0 +1,16 @@
+package terminal.user
+
+import services.UserService
+
+fun getUser(userService: UserService) {
+    println("Digite o id do usuário:")
+
+    val id = readln().toInt()
+
+    try {
+        val user = userService.getUser(id)
+        println(user.toString())
+    } catch (e: RuntimeException) {
+        e.printStackTrace()
+    }
+}
