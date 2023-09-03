87e61c929c6126059ca5ab7f05ef18ec6989c5ad
Eric Oliveira <oiericoi@hotmail.com>
2023-09-03 12:44:03 -0300

    feat: add EducationalContentNotFoundException error

--- src/main/kotlin/errors/HandlerException.kt
+++ src/main/kotlin/errors/HandlerException.kt
@@ -3,3 +3,4 @@ package errors
 class UserAlreadyExistsException: RuntimeException("User already exist.")
 class UserNotFoundException: RuntimeException("User not found")
 class AppErrorException: RuntimeException("Error starting the application.")
+class EducationalContentNotFoundException: RuntimeException("Educational content not found.")
