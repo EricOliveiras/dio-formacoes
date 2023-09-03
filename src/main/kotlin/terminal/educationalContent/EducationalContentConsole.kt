b3e9749a6856a3c32efed18d41565b24c44945da
Eric Oliveira <oiericoi@hotmail.com>
2023-09-03 12:52:12 -0300

    feat: add educational content console

---
+++ src/main/kotlin/terminal/educationalContent/EducationalContentConsole.kt
@@ -0,0 +1,36 @@
+package terminal.educationalContent
+
+import terminal.app
+import terminal.educationalContent.educationalContentService
+
+fun educationalContentConsole() {
+    println("|======== Conteúdos Educacionais ========|")
+    println("1 - Adicionar conteúdo")
+    println("2 - Buscar conteúdo")
+    println("3 - Buscar todos os conteúdos")
+    println("4 - Voltar ao menu principal.")
+
+    val choice = readln().toInt()
+
+    when (choice) {
+        1 -> {
+            createEducationalContent(educationalContentService)
+            educationalContentConsole()
+        }
+        2 -> {
+            getEducationalContent(educationalContentService)
+            educationalContentConsole()
+        }
+        3 -> {
+            getAllEducationalContent(educationalContentService)
+            educationalContentConsole()
+        }
+        4 -> {
+            app()
+        }
+        else -> {
+            println("Opção inválida. Tente novamente!")
+            educationalContentConsole()
+        }
+    }
+}
