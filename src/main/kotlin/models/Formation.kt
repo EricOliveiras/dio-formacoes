81e22b7ecac79463744bb5129ab7c2a32157dca4
Eric Oliveira <oiericoi@hotmail.com>
2023-09-04 10:54:23 -0300

    refactor: modify formation model

--- src/main/kotlin/models/Formation.kt
+++ src/main/kotlin/models/Formation.kt
@@ -2,4 +2,19 @@ package models
 
 import interfaces.Level
 
-data class Formation(val name: String, val level: Level, val educationalContent: List<EducationalContent>)
+data class Formation(
+    val id: Int,
+    val name: String,
+    val level: Level,
+    val educationalContent: MutableList<EducationalContent>,
+    val subscribed: MutableList<User>
+) {
+    fun enroll(user: User) {
+        if (!subscribed.contains(user)) {
+            subscribed.add(user)
+            println("${user.firstName} foi matriculado na formação $name.")
+        } else {
+            println("${user.firstName} já está matriculado na formação $name.")
+        }
+    }
+}
